9f396dbe7cf68ffccfca91ee2597a13ee0b4a425
samanthaaperry <113481983+samanthaaperry@users.noreply.github.com>
2024-12-04 17:59:39 -0600

    i dont think i even made changes to this but just being safe

--- Project_Assignments/Build_Scripts/init_build.sql
+++ Project_Assignments/Build_Scripts/init_build.sql
@@ -12,23 +12,31 @@ DROP TABLE IF EXISTS health_metrics;
 DROP TABLE IF EXISTS wellness_coordinator;
 DROP TABLE IF EXISTS wellness_program;
 DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS departments;
 
--- Re-Enable foreign key checks
+-- Re-enable foreign key checks
 SET FOREIGN_KEY_CHECKS = 1;
 
--- Create tables
+-- Create departments table first
+CREATE TABLE departments (
+    department_id VARCHAR(50) PRIMARY KEY,
+    department_name VARCHAR(100)
+);
 
+-- Create employee table
 CREATE TABLE employee (
     employee_id INT PRIMARY KEY, 
+    department_id VARCHAR(50),
     fname VARCHAR(20),
     middle_initial CHAR(1),
     lname VARCHAR(50),
     `role` ENUM('worker', 'secretary', 'coordinator'),
     phone_number VARCHAR(20),
-    department_id ENUM('sales', 'marketing', 'finance', 'operations'), -- (sales, marketing, finance, operations)
-    work_email VARCHAR(30)
+    work_email VARCHAR(30),
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
 
+-- Create wellness_coordinator table
 CREATE TABLE wellness_coordinator (
     employee_id INT PRIMARY KEY,
     area_of_expertise VARCHAR(20),
@@ -38,6 +46,7 @@ CREATE TABLE wellness_coordinator (
         ON UPDATE CASCADE
 );
 
+-- Create wellness_program table
 CREATE TABLE wellness_program (
     program_id INT PRIMARY KEY,
     employee_id INT,
@@ -45,11 +54,12 @@ CREATE TABLE wellness_program (
     program_name VARCHAR(20),
     start_date DATE,
     `type` ENUM('bmi', 'blood_pressure', 'heart_rate', 'cholesterol'),
-    FOREIGN KEY(employee_id) REFERENCES wellness_coordinator(employee_id)
+    FOREIGN KEY (employee_id) REFERENCES wellness_coordinator(employee_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
+-- Create participates_in table
 CREATE TABLE participates_in (
     employee_id INT,
     program_id INT,
@@ -63,10 +73,9 @@ CREATE TABLE participates_in (
         ON UPDATE CASCADE
 );
 
-
-
-
+-- Create health_metrics table
 CREATE TABLE health_metrics (
+    health_id INT PRIMARY KEY AUTO_INCREMENT,
     employee_id INT,
     date_measured DATE,
     cholesterol_levels INT,
@@ -74,12 +83,13 @@ CREATE TABLE health_metrics (
     blood_pressure_systolic INT,
     blood_pressure_diastolic INT,
     bmi FLOAT,
-    PRIMARY KEY (employee_id, date_measured),
+    UNIQUE KEY (employee_id, date_measured), -- Use UNIQUE instead of PRIMARY
     FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
+-- Create coordinated_by table
 CREATE TABLE coordinated_by (
     employee_id INT,
     program_id INT,
@@ -88,10 +98,9 @@ CREATE TABLE coordinated_by (
         ON DELETE NO ACTION
         ON UPDATE CASCADE,
     FOREIGN KEY (program_id) REFERENCES wellness_program(program_id)
-        ON DELETE CASCADE -- We will not allow for a coordinator to be deleted from the database.
-        ON UPDATE CASCADE -- It would delete all programs they manage. Updating their ID should be fine
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
-
 
 COMMIT;
 
